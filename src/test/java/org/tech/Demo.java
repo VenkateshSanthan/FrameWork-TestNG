b5117373f2f79b78119a55abcb4d967d84ddb888
Tesh <104306420+VenkateshSanthan@users.noreply.github.com>
2022-08-09 06:22:32 +0530

    Hotel Page

--- src/test/java/org/tech/Demo.java
+++ src/test/java/org/tech/Demo.java
@@ -2,23 +2,18 @@ package org.tech;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Demo {
 
 	public static WebDriver driver;
 
-@Test
 	private void demoQa() {
-		BaseClass b = new BaseClass();
-		WebDriver driver = b.launchChrome();
-		b.urlLaunch(driver, "https://demoqa.com/automation-practice-form");
-		b.fill(driver.findElement(By.id("firstName")),"Venkatesh");
-		b.fill(driver.findElement(By.id("lastName")),"Santhanam");
-		b.fill(driver.findElement(By.id("userEmail")), "venkatesg025@gmail.com");
 		
+		driver = new ChromeDriver();
 		
-		
+		driver.get("https://reqres.in//api/users?page=2");
 		
 		
 	}
--- src/test/java/org/tech/FaceBook_1.java
+++ src/test/java/org/tech/FaceBook_1.java
@@ -1,6 +1,7 @@
 package org.tech;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -10,20 +11,23 @@ public class FaceBook_1 {
 
 	
 	public static WebDriver driver;
+	private WebElement findElement;
 	
 @Test	
 	private void urlTest() {
-		BaseClass b = new BaseClass();
-		WebDriver driver = b.launchChrome();
-		b.urlLaunch(driver, "https://www.facebook.com/");
-		WebElement userName = driver.findElement(By.name("email"));
-		userName.sendKeys("9965675526");
-		Assert.assertEquals(userName.getAttribute("value"), "9965675526", "Verify UserName");
-		WebElement pass = driver.findElement(By.name("pass"));
-		pass.sendKeys("142475poli");
-		Assert.assertEquals(pass.getAttribute("value"), "142475polie", "Verify Password");
-		b.clickBtn(driver.findElement(By.name("login")));
 		
+		BaseClass b = new BaseClass();
+		b.launchChrome();
+		b.urlLaunch(driver, "https://www.facebook.com/");
+		
+		WebElement element = driver.findElement(By.id("email"));
+	
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('style,background:red')", element);
+		
+	
+	
+	
 	}
 	
 }
